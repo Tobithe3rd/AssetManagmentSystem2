cb54cd14364d44232bb02e11ba22525d25eff6ca
Tobithe3rd <160504065+Tobithe3rd@users.noreply.github.com>
2024-02-23 12:26:03 +0100

    Included API validation

--- Models/Asset.cs
+++ Models/Asset.cs
@@ -1,12 +1,15 @@
-﻿namespace AssetManagmentSystem2.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace AssetManagmentSystem2.Models
 {
     public class Asset
     {
         public int Id { get; set; }
-
+        [Required]
         public string Name { get; set;  }
-        public double Cost { get; set; }
-        public DateTime DateOfPurchase { get; set; }
+        [Required]
+    public double Cost { get; set; }
+        [Required]
+    public DateTime DateOfPurchase { get; set; }
         public Status Status { get; set; }
         public Category Category { get; set; }
     }
--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,15 @@
-﻿namespace AssetManagmentSystem2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AssetManagmentSystem2.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [EmailAddress]
         public string Email  { get; set; }
+        [Phone]
         public string PhoneNo {  get; set; }
     }
 }
